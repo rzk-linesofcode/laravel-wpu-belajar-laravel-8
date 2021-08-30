4fdc012bceb545c96f0070a7c203e7970a450065
rezky <rzk84.rezky@gmail.com>
2021-08-30 09:33:57 +0700

    lesson09

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -15,6 +15,10 @@
         <li class="nav-item">
           <a class="nav-link {{ $title === "Blog" ? 'active' : '' }}" href="/posts">Blog</a>
         </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link {{ $title === "Categories" ? 'active' : '' }}" href="/categories">Categories</a>
+      </li>        
       </ul>
     </div>
   </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
     <article>
         <h3>{{ $post->title }}</h3>
-        <p>By. Rezky in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
-    <a href="/posts">Back to Posts</a>
+    <a href="/posts" class="d-block mt-3">Back to Posts</a>
 @endsection
 
